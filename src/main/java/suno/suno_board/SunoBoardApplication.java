d939b654a84b81948b00f98584594f12ee52ac45
sunoyu <suno_yu@naver.com>
2025-04-20 13:03:04 +0900

    init

---
+++ src/main/java/suno/suno_board/SunoBoardApplication.java
@@ -0,0 +1,13 @@
+package suno.suno_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunoBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunoBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/suno/suno_board/SunoBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package suno.suno_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunoBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
